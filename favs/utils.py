f08fa160b6c8240531da9d46287ffafb858af58f
Nakamura Tatsuki <tatsuki@tatsuki.local>
2018-02-02 07:43:57 +0900

    Save before adding social-auth

--- favs/utils.py
+++ favs/utils.py
@@ -1,14 +1,19 @@
-from requests_oauthlib import OAuth1Session, OAuth1
 import json
 import os
-import urllib.parse
 import threading
+
 from queue import Queue
-import requests
+
 from favs.models import User
 
+from requests_oauthlib import OAuth1Session
+
+
 def merge_two_dicts(a, b):
-    return {**a, **b}
+    c = a.copy()
+    c.update(b)
+    return c
+
 
 class TwitterClient:
 
@@ -27,39 +32,44 @@ class TwitterClient:
         self.session = OAuth1Session(self.CK, self.CS, self.AT, self.AS)
 
         self.urls = {
-            'timeline': 'https://api.twitter.com/1.1/statuses/home_timeline.json',
+            'timeline':
+                'https://api.twitter.com/1.1/statuses/home_timeline.json',
             'favlist': 'https://api.twitter.com/1.1/favorites/list.json',
             'user': 'https://api.twitter.com/1.1/users/show.json',
             'oembed': 'https://publish.twitter.com/oembed',
             'request_token': 'https://twitter.com/oauth/request_token',
             'access_token': 'https://twitter.com/oauth/access_token',
             'authorize': 'https://twitter.com/oauth/authorize',
-            'account_verified': 'https://api.twitter.com/1.1/account/verify_credentials.json',
+            'account_verified':
+                'https://api.twitter.com/1.1/account/verify_credentials.json',
         }
 
     def timeline(self):
         res = self.session.get(self.urls['timeline'], params={})
-        if res.status_code != 200: return []
+        if res.status_code != 200:
+            return []
         return json.loads(res.text)
-        
+
     def favlist(self, user_id):
         params = {
             'user_id': user_id,
-            'count': 200,
+            'count': 20,
         }
         res = self.session.get(self.urls['favlist'], params=params)
-        if res.status_code != 200: return []
+        if res.status_code != 200:
+            return []
         return json.loads(res.text)
-    
+
     def user_from_screen_name(self, screen_name):
         params = {
             'screen_name': screen_name,
         }
-        res =  self.session.get(self.urls['user'], params=params)
+        res = self.session.get(self.urls['user'], params=params)
         print(res.text)
-        if res.status_code != 200: return {}
+        if res.status_code != 200:
+            return {}
         return json.loads(res.text)
-    
+
     def show_tweets(self, tweets):
         for item in tweets:
             print(item['text'])
@@ -75,13 +85,15 @@ class TwitterClient:
         return user['id_str'] if 'id_str' in user else ''
 
     def html_embedded(self, tweet, q):
-        url = 'https://twitter.com/{screen_name}/status/{tweet_id}'.format(screen_name=tweet['user']['screen_name'], tweet_id=tweet['id_str'])
+        url = 'https://twitter.com/{screen_name}/status/{tweet_id}'.format(
+            screen_name=tweet['user']['screen_name'], tweet_id=tweet['id_str'])
         params = {
             'url': url,
             'maxwidth': 300,
         }
         res = self.session.get(self.urls['oembed'], params=params)
-        if res.status_code != 200: return ''        
+        if res.status_code != 200:
+            return ''
         q.put(json.loads(res.text)['html'])
 
     def add_htmls_embedded(self, tweets):
@@ -98,14 +110,15 @@ class TwitterClient:
             th.join()
             tweet_add = merge_two_dicts(tweet, {'html_embedded': q.get()})
             tweets_add.append(tweet_add)
-        
+
         return tweets_add
 
     def issue_request_url(self):
         session_auth = OAuth1Session(self.CK, client_secret=self.CS)
         res = session_auth.fetch_request_token(self.urls['request_token'])
 
-        user = User(oauth_token=res['oauth_token'], oauth_secret=res['oauth_token_secret'])
+        user = User(oauth_token=res['oauth_token'],
+                    oauth_secret=res['oauth_token_secret'])
         user.save()
 
         return session_auth.authorization_url(self.urls['authorize'])
@@ -117,12 +130,17 @@ class TwitterClient:
         user = User.objects.get(oauth_token=oauth_token)
         oauth_secret = user.oauth_secret
 
-        session_auth = OAuth1Session(self.CK, client_secret=self.CS, resource_owner_key=oauth_token, resource_owner_secret=oauth_secret, verifier=oauth_verifier)
+        session_auth = OAuth1Session(self.CK,
+                                     client_secret=self.CS,
+                                     resource_owner_key=oauth_token,
+                                     resource_owner_secret=oauth_secret,
+                                     verifier=oauth_verifier)
         res = session_auth.fetch_access_token(self.urls['access_token'])
         user.access_token = res['oauth_token']
         user.access_secret = res['oauth_token_secret']
 
-        session_user = OAuth1Session(self.CK, self.CS, user.access_token, user.access_secret)
+        session_user = OAuth1Session(self.CK, self.CS,
+                                     user.access_token, user.access_secret)
         res = session_user.get(self.urls['account_verified'], params={})
         user_id = json.loads(res.text)['id_str']
         users_old = User.objects.filter(user_id=user_id)
@@ -131,23 +149,22 @@ class TwitterClient:
         user.user_id = user_id
         user.save()
         return user.user_id
-        
+
 if __name__ == '__main__':
 
     user_id = '1212759744'
     screen_name = 'kemomimi_oukoku'
-    
-    twitter = TwitterClient()    
+
+    twitter = TwitterClient()
 
     user = twitter.user_from_screen_name(screen_name)
-    #user_id = user['id_str']
+    # user_id = user['id_str']
     twitter.show_user(user)
 
-    
-    #tweets = twitter.timeline()
-    #tweets = twitter.favlist(user_id)
-    #twitter.show_tweets(tweets)
-    #tweets = twitter.add_htmls_embedded(tweets)
-    #print(tweets[0])
+    # tweets = twitter.timeline()
+    # tweets = twitter.favlist(user_id)
+    # twitter.show_tweets(tweets)
+    # tweets = twitter.add_htmls_embedded(tweets)
+    # print(tweets[0])
 
-    #print(twitter.issue_request_url())
+    # print(twitter.issue_request_url())
--- mysite/settings.py
+++ mysite/settings.py
@@ -92,16 +92,21 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        ('django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
@@ -126,7 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = (
-    #os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
 )
 
 # Simplified static file serving.
